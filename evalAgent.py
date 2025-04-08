9cdac25ae2a7871fb85920ccbf501420394c1275
Anshuman Dash <anshuman@ucsb.edu>
2025-04-07 18:07:17 -0700

    Definitive 1.303038138332256 EV

--- evalAgent.py
+++ evalAgent.py
@@ -1,14 +1,14 @@
 from agents import *
 from collections import Counter
-from rideTheBus import Game
+from rideTheBus import Game, Card
 from tqdm import tqdm
+from itertools import permutations
 
 
 game = Game()
 
 
 def sim(agent):
-    game.reset()
     EV = 1
     act1 = agent.round1(game.chosen[:0])
     newEV = game.round1(act1)
@@ -44,17 +44,35 @@ def sim(agent):
     return EV
 
 
+def treeSearch(agent):
+    DECK = [Card(suit, val) for suit in "SHDC" for val in range(1, 14)]
+
+    total_runs = 0
+    total_EV = 0
+    for chosen in tqdm(permutations(DECK, 4), total=52 * 51 * 50 * 49):
+        game.chosen = list(chosen)
+        game.cards = list(chosen) + [c for c in DECK if c not in chosen]
+        # print(game.chosen, game.cards)
+
+        total_EV += sim(agent)
+        total_runs += 1
+    return total_EV / total_runs
+
+
 if __name__ == "__main__":
     agent = StatsAgent(r2Threshold=16, r3Threshold=24)
-    iters = 100000
-    # iters = 1
-    rew_sum = 0
-    counts = Counter()
-    for _ in tqdm(range(iters)):
-        res = sim(agent)
-        counts[res] += 1
-        rew_sum += res
-    print("EV:", rew_sum / iters)
+    # iters = 100000
+    # # iters = 1
+    # rew_sum = 0
+    # counts = Counter()
+    # for _ in tqdm(range(iters)):
+    #     game.reset()
+    #
+    #     res = sim(agent)
+    #     counts[res] += 1
+    #     rew_sum += res
+    print(treeSearch(agent))
+    # print("EV:", rew_sum / iters)
     # print(counts)
     print()
-    game.printStats()
+    # game.printStats()
